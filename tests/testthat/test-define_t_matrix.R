0ab9cecff713d0527cce906ff71d3e81f86362b5
Wael Mohammed <wmamohammed1@sheffield.ac.uk>
2023-04-20 15:18:09 +0100

    added define transition matrixfunction unit-test

--- tests/testthat/test-define_t_matrix.R
+++ tests/testthat/test-define_t_matrix.R
@@ -1,3 +1,23 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that(desc = "transition matrix definition works", {
+  ## Define expected outputs:
+  expected_output <- matrix(
+    data = c(0.845, 0.15, 0, 0.005,
+             0.5, 0.3800749, 0.105, 0.01492512,
+             0, 0, 0.9511101, 0.04888987,
+             0, 0, 0, 1),
+    nrow = 4,
+    byrow = TRUE,
+    dimnames = list(
+      c("H", "S1", "S2", "D"),
+      c("H", "S1", "S2", "D")
+    )
+  )
+
+  ## Define outputs:
+  output <- define_t_matrix()
+
+  ## Run tests:
+  expect_equal(expected_output, output, tolerance = 0.00001)
+  expect_error(define_t_matrix(p_HD = 1.1))
+  expect_error(define_t_matrix(p_HD = -0.2))
 })
